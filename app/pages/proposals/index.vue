f2ba7de608601172f6c3b53d998de206c9d90d3e
NicholasTechmoverai <techmoversai@gmail.com>
2026-04-22 20:32:16 +0300

    added proposals and project slugs

--- app/pages/proposals/index.vue
+++ app/pages/proposals/index.vue
@@ -34,7 +34,7 @@ useSeoMeta({
         <UPageSection :ui="{
             container: 'pt-0!'
         }">
-            <UBlogPosts v-if="posts" orientation="vertical">
+            <UBlogPosts v-if="posts?.length > 0" orientation="vertical">
                 <Motion v-for="(post, index) in posts" :key="index"
                     :initial="{ opacity: 0, transform: 'translateY(10px)' }"
                     :while-in-view="{ opacity: 1, transform: 'translateY(0)' }" :transition="{ delay: 0.2 * index }"
